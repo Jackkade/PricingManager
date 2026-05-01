910649f1a72452ca683e3d04dcaa4cff3db8f503
Jackkade <mmklop.mmklop@gmail.com>
2026-04-30 19:32:36 -0500

    Updated UI, added costEntry class

---
+++ src/data/costEntry.h
@@ -0,0 +1,15 @@
+#include <string>
+using std::string;
+
+class costEntry {
+
+    private:
+    
+    string partID;                  //MAX 8 Char
+    int supID;
+    string partColor = "--";        //Max 2 Char
+    int materialCost;
+
+    public:
+
+};
