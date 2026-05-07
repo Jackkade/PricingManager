495dc0973c3b831333890fe98b55fc86e1d9f3b4
Jackkade <mmklop.mmklop@gmail.com>
2026-05-07 17:27:43 -0500

    Max for file name input is now 8 characters

--- src/main/mainwindow.cpp
+++ src/main/mainwindow.cpp
@@ -2,16 +2,23 @@
 #include "ui_mainwindow.h"
 #include <iostream>
 #include "costEntry.cpp"
-
+#include <QFile>
+#include <QStandardPaths>
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
+
+    QFile file(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "\\pricingfile.txt");
+
+    if (!file.open(QIODevice::ReadWrite)) {
+        QMessageBox::information(0, "error", file.errorString());
+    }
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
@@ -21,7 +28,7 @@ void MainWindow::on_btnAdd_clicked() {
     if (!ui->addEntryInput->text().isEmpty()) {
 
         //QListWidgetItem* item = new QListWidgetItem(ui->addEntryInput->text());
-        QListWidgetItem* item = new QListWidgetItem(QString::fromStdString((CostEntry("AAABBBCC", 15, "--", "TP", Money(100000), Money(0), 5)).getStandardForm()));
+        QListWidgetItem* item = new QListWidgetItem(QString::fromStdString(CostEntry(ui->addEntryInput->text().toStdString(), 15, "--", "TP", Money(100000), Money(0), 5).getStandardForm()));
         item->setFlags(item->flags() | Qt::ItemIsEditable);
         ui->listWidget->addItem(item);
         ui->addEntryInput->clear();
@@ -35,6 +42,7 @@ void MainWindow::on_btnRemove_clicked() {
         delete item;
     }
 }
+
 void MainWindow::on_btnRemoveAll_clicked() {
     ui->listWidget->clear();
 }
