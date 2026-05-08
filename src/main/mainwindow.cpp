e2c616dd878c9a92f76a605e1a8eb96ca147a533
Jackkade <mmklop.mmklop@gmail.com>
2026-05-07 21:12:08 -0500

    Added File IO to project

--- src/main/mainwindow.cpp
+++ src/main/mainwindow.cpp
@@ -11,28 +11,65 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->setupUi(this);
 
+    QFile file(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "//pricingfile.txt");
+
+    if (!file.open(QIODevice::ReadWrite)) {
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file);
+
+    while (!in.atEnd()) {
+        QListWidgetItem* item = new QListWidgetItem(in.readLine(), ui->listWidget);
+        ui->listWidget->addItem(item);
+        item->setFlags(item->flags() | Qt::ItemIsEditable);
+    }
+    file.close();
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+
+
     QFile file(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "\\pricingfile.txt");
 
     if (!file.open(QIODevice::ReadWrite)) {
         QMessageBox::information(0, "error", file.errorString());
     }
-}
 
-MainWindow::~MainWindow() {
-    delete ui;
+    QTextStream out(&file);
+
+    for (int i = 0; i < ui->listWidget->count(); ++i) {
+        out << ui->listWidget->item(i)->text() << '\n';
+    }
+
+    file.close();
 }
 
 
 void MainWindow::on_btnAdd_clicked() {
-    std::cout << ui->addEntryInput->text().toStdString() << "\n";
-    if (!ui->addEntryInput->text().isEmpty()) {
-
+    std::cout << ui->addEntryFileName->text().toStdString() << "\n";
+    if (!ui->addEntryFileName->text().isEmpty()) {
+        string partColorStr = "--";
+        if (!ui->addEntryColor->text().isEmpty()) {
+            partColorStr = ui->addEntryColor->text().toStdString();
+        }
         //QListWidgetItem* item = new QListWidgetItem(ui->addEntryInput->text());
-        QListWidgetItem* item = new QListWidgetItem(QString::fromStdString(CostEntry(ui->addEntryInput->text().toStdString(), 15, "--", "TP", Money(100000), Money(0), 5).getStandardForm()));
+        QListWidgetItem* item = new QListWidgetItem(QString::fromStdString(
+            CostEntry(
+                ui->addEntryFileName->text().toStdString(),
+                ui->addEntrySupID->value(),
+                partColorStr,
+                ui->addEntryCostUnit->currentText().toStdString(),
+                Money(100000),
+                Money(0),
+                ui->addEntryMinUnits->value()
+            ).getStandardForm()
+        ));
         item->setFlags(item->flags() | Qt::ItemIsEditable);
         ui->listWidget->addItem(item);
-        ui->addEntryInput->clear();
-        ui->addEntryInput->setFocus();
+        ui->addEntryFileName->clear();
+        ui->addEntryFileName->setFocus();
     }
 }
 
